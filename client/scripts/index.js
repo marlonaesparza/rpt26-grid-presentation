0a420b711ca44647ce5ef52983931aa1acbd8de2
Marlon Esparza <marlonaesparza@gmail.com>
2021-01-29 23:26:57 -0800

    Init

---
+++ client/scripts/index.js
@@ -0,0 +1,11 @@
+function initiateVideos () {
+  let videoContainer = document.getElementById('latest-videos');
+  let video = document.getElementsByClassName('video')[0];
+
+  for (let i = 0; i < 100; i++) {
+    let videoClone = video.cloneNode(true);
+    videoContainer.appendChild(videoClone);
+  }
+}
+
+initiateVideos();
